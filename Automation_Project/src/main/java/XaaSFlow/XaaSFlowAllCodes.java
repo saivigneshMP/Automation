052e6cc53a40eeab7c06a9160de90a7399a6b616
saivignesh <saivignesh@duster.chncorent.com>
2024-06-28 10:05:19 +0530

    test

--- Automation_Project/src/main/java/XaaSFlow/XaaSFlowAllCodes.java
+++ Automation_Project/src/main/java/XaaSFlow/XaaSFlowAllCodes.java
@@ -1,6 +1,7 @@
 package XaaSFlow;
 
 import java.time.Duration;
+import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -21,10 +22,19 @@ public class XaaSFlowAllCodes {
 	    driver.manage().window().maximize();
 	    driver.get("https://automationzone.qaops.smartersaas.com:9000/surpaas/#");
 	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='loginForm:username' or @name = 'loginForm:username']"))).sendKeys("saivignesh@corenttech.com");
-	 //   driver.findElement(By.xpath("//input[@id='loginForm:username' or @name = 'loginForm:username']")).sendKeys("saivignesh@corenttech.com");
 	    driver.findElement(By.xpath("//input[@id='loginForm:password' or @name = 'loginForm:password']")).sendKeys("Corent@123$#");
 	    driver.findElement(By.xpath("//span[contains(text(),'Sign In')]")).click();
 	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@id='menuCLinkForm:appsaascCmdLink']"))).click();
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='Open_create_application_profile_btn']"))).click();
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='radio-3']/following-sibling::label"))).click();
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='selectsaasprofileFlowForm:create_application_profile_btn_slid1']"))).click();
+	    Random r = new Random();
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='profilecreationForm:existingClustername_input']"))).sendKeys("XaaSgroup" + r.nextInt(456));
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='profilecreationForm:profilename']"))).sendKeys("XaaSapplication" + r.nextInt(456));
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='profilecreationForm:create_application_profile_sidebar']"))).click();
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='profilecreationForm:existingAppusername_input']"))).sendKeys("XaaSapplication" + r.nextInt(456));
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='profilecreationForm:profilename']"))).sendKeys("XaaSapplication" + r.nextInt(456));
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='profilecreationForm:create_application_profile_sidebar']"))).click();
 	}
 
 }
