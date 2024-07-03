cfea87c4349c4aee46cac899aca52f6be1901802
saivignesh <saivignesh@duster.chncorent.com>
2024-07-03 18:39:47 +0530

    test

--- Automation_Project/src/main/java/XaaSFlow/XaaSFlowAllCodes.java
+++ Automation_Project/src/main/java/XaaSFlow/XaaSFlowAllCodes.java
@@ -4,16 +4,14 @@ import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.time.Duration;
+import java.util.List;
 import java.util.Random;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
@@ -30,7 +28,7 @@ public class XaaSFlowAllCodes {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		WebDriverWait loginwait = new WebDriverWait(driver, Duration.ofMinutes(1));
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
 		Wait<WebDriver> fwait = new FluentWait<>(driver).withTimeout(Duration.ofSeconds(40))
 				.pollingEvery(Duration.ofSeconds(3)).ignoring(StaleElementReferenceException.class);
 		Actions a = new Actions(driver);
@@ -51,7 +49,7 @@ public class XaaSFlowAllCodes {
 				.click();
 		wait.until(ExpectedConditions.elementToBeClickable(
 				By.xpath("//button[@id='selectsaasprofileFlowForm:create_application_profile_btn_slid1']"))).click();
-		Random r = new Random();		
+		Random r = new Random();
 		fwait.until(ExpectedConditions
 				.elementToBeClickable(By.xpath("//input[@id='profilecreationForm:existingClustername_input']")))
 				.sendKeys("groupname" + r.nextInt());
@@ -60,13 +58,14 @@ public class XaaSFlowAllCodes {
 				.sendKeys("applicationname" + r.nextInt());
 		fwait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='profilecreationForm:profilename']")))
 				.sendKeys("profilename" + r.nextInt());
-		Thread.sleep(5000);
+		Thread.sleep(10000);
 		String applicationName = fwait
 				.until(ExpectedConditions
 						.elementToBeClickable(By.xpath("//input[@id='profilecreationForm:existingAppusername_input']")))
 				.getAttribute("value");
-		String profileName = fwait.until(
-				ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='profilecreationForm:profilename']")))
+		String profileName = fwait
+				.until(ExpectedConditions
+						.elementToBeClickable(By.xpath("//input[@id='profilecreationForm:profilename']")))
 				.getAttribute("value");
 		if (applicationName.isEmpty()) {
 			fwait.until(ExpectedConditions
@@ -82,8 +81,9 @@ public class XaaSFlowAllCodes {
 		} else {
 
 		}
-		String applicationNameNew =fwait.until(ExpectedConditions
-				.elementToBeClickable(By.xpath("//input[@id='profilecreationForm:existingAppusername_input']")))
+		String applicationNameNew = fwait
+				.until(ExpectedConditions
+						.elementToBeClickable(By.xpath("//input[@id='profilecreationForm:existingAppusername_input']")))
 				.getAttribute("value");
 		wait.until(ExpectedConditions.elementToBeClickable(
 				By.xpath("//button[@id='profilecreationForm:create_application_profile_sidebar']"))).click();
@@ -100,34 +100,88 @@ public class XaaSFlowAllCodes {
 				.click();
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='statusDialogform:Ok']"))).click();
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//i[@class='fa fa-chevron-right'])[1]"))).click();
-		
+
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//iframe[@class='cke_wysiwyg_frame cke_reset']")));
-	 	WebElement iframe = driver.findElement(By.xpath("//iframe[@class='cke_wysiwyg_frame cke_reset']"));
-	    driver.switchTo().frame(iframe);
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='cke_editable cke_editable_themed cke_contents_ltr cke_show_borders']/p")))
-		.sendKeys("test" + r.nextInt());
+		WebElement iframe = driver.findElement(By.xpath("//iframe[@class='cke_wysiwyg_frame cke_reset']"));
+		driver.switchTo().frame(iframe);
+		wait.until(ExpectedConditions.elementToBeClickable(
+				By.xpath("//*[@class='cke_editable cke_editable_themed cke_contents_ltr cke_show_borders']/p")))
+				.sendKeys("test" + r.nextInt());
 		driver.switchTo().defaultContent();
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='applicationArtifaactAddEditForm:saveButton']")))
-		.click();
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='statusDialogform:Ok']")))
-		.click();
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//i[@class='fa fa-chevron-right'])[4]")))
-    	.click();		
+		wait.until(ExpectedConditions
+				.elementToBeClickable(By.xpath("//button[@id='applicationArtifaactAddEditForm:saveButton']"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='statusDialogform:Ok']"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//i[@class='fa fa-chevron-right'])[4]"))).click();
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@id='menuCLinkForm:appMarketizercCmdLink']")))
-    	.click();		
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='appandoffersetupHomeListForm:j_idt5147']")));
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@id='collapse-icon']")))
-    	.click();
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='appandoffersetupHomeListForm:j_idt5147']"))).click();		
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='appcreatestatusdialog:existingAppName_input']")))
-    	.click();
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='appcreatestatusdialog:existingAppName_button']")))
-    	.click();
-		driver.findElement(By.xpath("//span[@id='appcreatestatusdialog:existingAppName']/input")).sendKeys(applicationNameNew);
-		driver.findElement(By.xpath("//button[@id='appcreatestatusdialog:advisoryregionsave']")).click();		
+				.click();
+		wait.until(ExpectedConditions.elementToBeClickable(
+				By.xpath("//div[@id='appandoffersetupHomeListForm:appandoffersetupHomePanel']//button")));
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@id='collapse-icon']"))).click();
+		List<WebElement> planNames = driver.findElements(By.xpath("//span[@class='gui-inline-header']"));
+		for (int i = 0; i < planNames.size(); i++) {
+			WebElement element = planNames.get(i);
+			if (element.getText().contains("xaasapplication21")) {
+				element.click();
+
+			}
+
+		}
+
+		wait.until(ExpectedConditions
+				.elementToBeClickable(By.xpath("//button[@id='subscriptionPlanForm:subscriptionAddid']/span"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='AddorEditForm:productName']"))).sendKeys("plan"+ r.nextInt());
+		WebElement iframe1 = driver.findElement(By.xpath("//iframe[@class='cke_wysiwyg_frame cke_reset']"));
+		driver.switchTo().frame(iframe1);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='cke_editable cke_editable_themed cke_contents_ltr cke_show_borders']/p"))).click();
+		//wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='cke_editable cke_editable_themed cke_contents_ltr cke_show_borders']/p"))).sendKeys("test"+ r.nextInt());
+		driver.switchTo().defaultContent();
+		Robot robot = new Robot();
+		robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+		Thread.sleep(2000);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='AddorEditForm:saasBox']//span"))).click();
+		List<WebElement> saasbox = driver.findElements(By.xpath("//ul[@id='AddorEditForm:saasBox_items']/li"));
+		for(WebElement SaaSbox : saasbox) {
+			if(SaaSbox.getText().contains("Default")) {
+				SaaSbox.click();
+			}
+			
+		}
 		
-		
+		Thread.sleep(2000);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='AddorEditForm:paymentGatewaytype']//span"))).click();
+		List<WebElement> paymentGateway = driver.findElements(By.xpath("//ul[@id='AddorEditForm:paymentGatewaytype_items']//li"));
+		for(WebElement payment : paymentGateway) {
+			if(payment.getText().contains("Default")) {
+				payment.click();
+			}
+			
+		}
 	
+		Thread.sleep(2000);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='AddorEditForm:currencychoose']//span"))).click();
+		List<WebElement> currencies = driver.findElements(By.xpath("//ul[@id='AddorEditForm:currencychoose_items']//li"));
+		for(WebElement currency : currencies) {
+			if(currency.getText().contains("USD: United States Dollar($)")) {
+				currency.click();
+			}
+			
+		}
+		Thread.sleep(2000);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='AddorEditForm:recurringTypeid']//span"))).click();
+		List<WebElement> recuuring = driver.findElements(By.xpath("//ul[@id='AddorEditForm:recurringTypeid_items']//li"));
+		for(WebElement recuuring_every : recuuring) {
+			if(recuuring_every.getText().contains("Daily")) {
+				recuuring_every.click();
+			}
+			
+		}
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='AddorEditForm:productSaveOrUpdate']"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='SuccessFormsummary:successcloseButn']"))).click();
+	    String tenantURL = 	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@id='subscriptionSummaryHomeForm:subscriptiondataGridId:0:subscriptiondataGridPanel_content']//a)[1]"))).getAttribute("href");
+	    driver.get(tenantURL);
+	   System.out.println(driver.getCurrentUrl());
 	}
+	
+	
 
 }
